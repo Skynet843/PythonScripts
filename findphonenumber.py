5428f1e273e727b868cc1c38ba021bf65fd994b1
Amit Bansal <amitbansal@Amit.local>
2016-12-24 23:14:30 +0530

    MakeAQuiz

--- findphonenumber.py
+++ findphonenumber.py
@@ -1,26 +1,7 @@
-def ifphonenumber(no):
-	if len(no)!= 10:
-		return False
+import re
 
-	for i in range(len(no)):
-		if no[i].isspace():
-			return False	
-	try: 
-		no = int(no)
-	except:
-		return False
-
-	return True
-
+phoneno = re.compile(r'\d\d\d\d\d\d\d\d\d\d')
 message = input()
-for i in range(len(message)):
-	temp = message[i:i+10]
-	if ifphonenumber(temp):
-		print("Phone number found :" + temp)
-
-
-
-
-
-
-
+new = phoneno.findall(message)
+for i in new:
+	print("phone number found : "+i)
